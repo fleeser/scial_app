aaa6c05e3cee0a44b3bf1d37099118652ddd25d6
Florian Leeser <florian.leeser@icloud.com>
2023-04-18 22:06:51 +0200

    :wheelchair: Made use of path url strategy for web

--- scial_app_flutter/lib/main.dart
+++ scial_app_flutter/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:scial_app_client/scial_app_client.dart';
@@ -8,11 +7,14 @@ import 'package:scial_app_flutter/src/services/auth_key_manager.dart';
 import 'package:scial_app_flutter/src/services/key_value_storage.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 late Client client;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  usePathUrlStrategy();
 
   await dotenv.load(fileName: '.env');
 
